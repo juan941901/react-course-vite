9a6fe3fafb3f7ea86633e4e937495f0a4fba36aa
Juan David Cordoba Ortega <juan.cordoba@tucuota.co>
2024-04-01 11:07:09 -0500

    Primer commit

---
+++ src/Pages/MyAccount/index.jsx
@@ -0,0 +1,14 @@
+
+function MyAccount() {
+
+    return (
+      <>
+        <div className="bg-red-100">
+        MyAccount
+        </div>
+      </>
+    )
+  }
+  
+  export default MyAccount
+  
