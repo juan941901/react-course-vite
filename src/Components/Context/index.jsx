a7173ecd4aa06e1bebfd93f1cc9be7f7c20c6d52
Juan David Cordoba Ortega <juan.cordoba@tucuota.co>
2024-04-19 13:22:59 -0500

    configuramos para abrir y cerrar vista con descripción del producto al igual que organizamos su contenido html para la visualización

--- src/Components/Context/index.jsx
+++ src/Components/Context/index.jsx
@@ -4,13 +4,26 @@ export const ShopingCarContext = createContext()
 
 export const ShopingCarProvider = ({children}) => {
 
+    //Prodcut Detail - Increment quantity
     const [count, setCount] = useState(0)
+
+    //Prodcut Detail - Open/close
+    const [isOpenProductDetail, setIsOpenProductDetail] = useState(false)
+    const openProductDetail = () => setIsOpenProductDetail(true)
+    const closeProductDetail = () => setIsOpenProductDetail(false)
     
+    //Prodcut Detail - Show Product
+    const [productToShow, setProductToShow] = useState({})
 
     return(
         <ShopingCarContext.Provider value={{
             count,
-            setCount
+            setCount,
+            isOpenProductDetail,
+            openProductDetail,
+            closeProductDetail,
+            productToShow,
+            setProductToShow
         }}>
             {children}
         </ShopingCarContext.Provider>
--- src/Components/ProductDetail/index.jsx
+++ src/Components/ProductDetail/index.jsx
@@ -1,19 +1,37 @@
+import { useContext } from 'react'
 import { XCircleIcon } from '@heroicons/react/24/solid'
+import { ShopingCarContext } from '../Context'
 import './style.css'
 
 const ProductDetail = () => {
 
+    const context = useContext(ShopingCarContext)
+    
     return (
 
-        <aside className='product-detail flex flex-col fixed right-0 border border-black rounded-lg bg-white'>
+        <aside className={`${context.isOpenProductDetail ? 'flex' : 'hidden'} product-detail flex-col fixed right-0 border border-black rounded-lg bg-white`}>
             <div className='flex justify-between items-center p-6'>
                 <h2 className='font-medium text-xl'>
                     Detail
                 </h2>
                 <div>
-                    <XCircleIcon className='h-6 w-6 text-black'></XCircleIcon>
+                    <XCircleIcon 
+                        className='h-6 w-6 text-black cursor-pointer'
+                        onClick={()=>context.closeProductDetail()} 
+                    ></XCircleIcon>
                 </div>
             </div>
+            <figure className='px-6'>
+                    <img
+                        className='w-full h-full rounded-lg' 
+                        src={context.productToShow.images}
+                        alt={context.productToShow.title}  />
+                </figure>
+                <p className='flex flex-col p-6'>
+                    <span className='font-medium text-2xl mb-2'>${context.productToShow.price}</span>
+                    <span className='font-medium text-md'>{context.productToShow.title}</span>
+                    <span className='font-light text-sm'>{context.productToShow.description}</span>
+                </p>
         </aside>
 
     )
